c3de4b392d3772875405b58da4e26e6ee42851aa
Petar Taushanov <pepi15032000@gmail.com>
2020-07-27 21:22:00 +0300

    Updated list command

--- models/commands/list.js
+++ models/commands/list.js
@@ -1,3 +1,13 @@
+const mongo = require("mongodb");
+
+//Importing .env configurations
+require('dotenv').config();
+
+// Database using MongoDB
+const MongoClient = mongo.MongoClient;
+const URI = process.env.DB_URI;
+const DB = process.env.DB_NAME;
+
 exports.list = (socket, ...args) => {
 
     if (args.length < 1) {
@@ -8,7 +18,7 @@ exports.list = (socket, ...args) => {
         return;
     }
 
-    if (args[0] === "users" || args[0] === "Users") {
+    if (args[0].toLowerCase() === "users") {
         let usernames, message = `List of online users:\n`;
 
         switch (args.length) {
@@ -23,10 +33,9 @@ exports.list = (socket, ...args) => {
                 )
                 break;
         }
-        console.log(usernames)
 
         for (let username of usernames) {
-            message += "○ " + username + "\n"
+            message += "❊ " + username + "\n"
         }
 
         message += `\n Total count of listed users: ${usernames.size}.`
@@ -42,6 +51,43 @@ exports.list = (socket, ...args) => {
                 message: "Error! You are not logged in to use this command!"
             })
         }
+    } else if (args[0].toLowerCase() === "channels") {
+        const client = new MongoClient(URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        client.connect((err) => {
+            const db = client.db(DB);
+
+            if (err) {
+                console.error(err);
+            }
+
+            db.collection("Channels")
+                .find()
+                .project({
+                    _id: 0,
+                    name: 1
+                })
+                .toArray()
+                .then(res => {
+                    const names = res.map(x => x.name);
+                    let message = "List of all channels:\n"
+                    names.forEach(name => {
+                        message += "❊ " + name + '\n'
+                    })
+                    socket.emit("info-message", {
+                        from: "🛈Info",
+                        message: message
+                    })
+                })
+                .catch(err => {
+                    console.error(err);
+                    client.close();
+                })
+        })
+        client.close();
     } else {
         socket.emit("server-error", {
             from: "System",
